765e85f35cd61283856846296a6343fb97e49b54
Krishna-sm <krishna.web@gmail.com>
2025-05-04 13:19:47 +0530

    final

--- frontend/src/app/not-found.jsx
+++ frontend/src/app/not-found.jsx
@@ -1,8 +1,19 @@
+ 
+import Link from 'next/link'; 
 import React from 'react'
 
 const ErrorPage = () => {
+ 
+
   return (
-    <div>ErrorPage</div>
+      <>
+    <div className=" my-10 w-[96%] py-10 lg:w-1/2 flex items-center mx-auto flex-col justify-center lg:shadow lg:border rounded-md gap-y-5">
+    <img src="/error.webp" alt="" />
+            <h1 className='text-4xl font-bold'>Not Found</h1>
+            <Link  href={'/'} className='px-3 py-2 bg-rose-600 hover:bg-rose-800 transition-all duration-300 text-white rounded cursor-pointer'>Go To Dashboard</Link>
+    </div>
+
+      </>
   )
 }
 
